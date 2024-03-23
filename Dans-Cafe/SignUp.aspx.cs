e0877136488d825888df6662d904c49b2282a77b
Asteropeia <154642650+LiyahGutz@users.noreply.github.com>
2024-03-23 22:44:38 +0800

    Added Dans-Cafe with Database, SignUp, Login, Home

---
+++ Dans-Cafe/SignUp.aspx.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections;
+using Login.App.Users;
+using System.Linq;
+
+namespace DansCafe
+{
+    public partial class SignUp : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ValidationSettings.UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+        }
+
+        protected void submitBtn_Click(object sender, EventArgs e)
+        {
+            if (validSignUp())
+            {
+                string newEmail = Email.Text;
+
+                if (!ExistUser(newEmail))
+                {
+                    var userRepo = new RepositoryUsers();
+
+                    userRepo.NewUser(new User()
+                    {
+                        FirstName = Fname.Text,
+                        LastName = Lname.Text,
+                        Email = Email.Text,
+                        Username = Username.Text,
+                        Password = Password.Text,
+                    });
+
+                    ClearValues();
+
+                    Response.Write("<script>alert('You are now registered!');</script>");
+                    Response.Write("<script>window.setTimeout(function(){ window.location.href = 'login.aspx'; }, 1000);</script>");
+                } 
+                else
+                {
+                    string alreadyExist = "Email already used.";
+                    Response.Write($"<script>alert('{alreadyExist}')</script>");
+                    ClearValues();
+                }
+            }
+                
+                
+            
+        }
+
+        protected bool validSignUp()
+        {
+            if (Page.IsValid)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        protected void ClearValues()
+        {
+            Fname.Text = "";
+            Lname.Text = "";
+            Email.Text = "";
+            Username.Text = "";
+            IniPword.Text = "";
+            Password.Text = "";
+        }
+
+        protected bool ExistUser(string newUser)
+        {
+            RepositoryUsers repos = new RepositoryUsers();
+            IEnumerable<User> userPeople = repos.GetAllUsers();
+
+            return userPeople.Any(user => user.Email == newUser);
+        }
+    }
+}
---
+++ Dans-Cafe/login.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using Login.App.Users;
+
+
+namespace DansCafe
+{
+    public partial class login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ValidationSettings.UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+        }
+
+        protected void loginBtn_Click(object sender, EventArgs e)
+        {
+            var userRepo = new RepositoryUsers();
+
+            var currentUser = userRepo.LogIn(userNtxtbox.Text);
+
+            if (currentUser != null && currentUser.Any())
+            {
+                foreach (var user in currentUser)
+                {
+                    if (user.Username != null)
+                    {
+                        if (user.Password == passtxtbox.Text)
+                        {
+                            Response.Write("<script>window.setTimeout(function(){ window.location.href = 'home.aspx'; }, 1000);</script>");
+                        }
+                        else
+                        {
+                            string wrongPass = "Incorrect Password";
+                            Response.Write($"<script>alert('{wrongPass}')</script>");
+                        }
+                    }
+                    else
+                    {
+                        string notUser = "User does not exist.";
+                        Response.Write($"<script>alert('{notUser}')</script>");
+                    }
+                }
+            }
+
+            //var connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"D:\\Aaliyah Gutierrez\\source\\repos\\Login\\Login\\App_Data\\Users.mdf\";Integrated Security=True";
+
+            //using (var connection = new SqlConnection(connectionString))
+            //{
+            //    connection.Open();
+            //}
+        }
+    }
+}
