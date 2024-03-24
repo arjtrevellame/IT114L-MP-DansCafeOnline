66a1b3b4acfb6f80b746cc50e588d2c9b7243d2d
Asteropeia <154642650+LiyahGutz@users.noreply.github.com>
2024-03-24 14:43:42 +0800

    added DansCafe

---
+++ Dans_Cafe/App/Users/RepositoryUsers.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace Dans_Cafe.App.Users
+{
+    public class RepositoryUsers
+    {
+        public string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""D:\Aaliyah Gutierrez\source\repos\Dans_Cafe\Dans_Cafe\App_Data\UserSchema.mdf"";Integrated Security=True";
+
+        public void NewUser(User user)
+        {
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                SqlCommand insertCommand = new SqlCommand("INSERT INTO RegisteredUsers VALUES('"
+                    + user.FirstName
+                    + "','" + user.LastName
+                    + "','" + user.Username
+                    + "','" + user.Email
+                    + "','" + user.Password
+                    + "');", connection);
+
+                insertCommand.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+
+        public IEnumerable<User> LogIn(string username)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            using (var logCommand = connection.CreateCommand())
+            {
+                connection.Open();
+
+                string loginData = "SELECT UserName, Password FROM RegisteredUsers WHERE UserName = @username";
+                logCommand.CommandText = loginData;
+                logCommand.Parameters.AddWithValue("@username", username);
+
+                return logCommand
+                    .ExecuteReader()
+                    .Cast<IDataRecord>()
+                    .Select(row => new User()
+                    {
+                        Username = (string)row["UserName"],
+                        Password = (string)row["Password"]
+                    })
+                    .ToList();
+            }
+        }
+
+        public IEnumerable<User> GetAllUsers()
+        {
+            using (var connection = new SqlConnection(connectionString))
+            using (var logCommand = connection.CreateCommand())
+            {
+                connection.Open();
+
+                string getData = "SELECT * FROM RegisteredUsers";
+                logCommand.CommandText = getData;
+
+                return logCommand
+                    .ExecuteReader()
+                    .Cast<IDataRecord>()
+                    .Select(row => new User()
+                    {
+                        Username = (string)row["UserName"],
+                        Email = (string)row["Email"],
+                    })
+                    .ToList();
+            }
+        }
+    }
+}
